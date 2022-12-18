ca208be2fef29cd7f504196d9fd5c2975ee17ad6
Gabriel Oliveira <gboliveira2503@gmail.com>
2022-12-18 18:48:49 -0300

    settings

--- bandkamp/settings.py
+++ bandkamp/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG",False)
 
-ALLOWED_HOSTS = ["deploy-entrega-production.up.railway.app","0.0.0.0"]
+ALLOWED_HOSTS = ["deploy-entrega-production.up.railway.app"]
 
 
 # Application definition
